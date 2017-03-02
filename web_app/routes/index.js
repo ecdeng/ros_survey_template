feecea90de6443821062f956978058226cfcde90
Michelle Becerra <michel8@vt.edu>
2017-03-01 23:15:19 -0800

    html and css

--- web_app/routes/index.js
+++ web_app/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+var checkedVal = null;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ROS Survey', des: 'web skeleton' });
+  res.render('index', { title: 'Child Behavior Checklist'});
 });
+
+// var inputElements = document.getElementsByTypeName('checkbox');
+// for(var i=0; inputElements[i]; ++i){
+//       if(inputElements[i].checked){
+//            checkedVal = inputElements[i].value;
+//            console.log(checkedVal)
+//            break;
+//       }
+// }
 
 module.exports = router;
